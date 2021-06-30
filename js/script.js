8e3632338d91b37ef15b9853485c09cf51935fbd
Boffin-ux <fnamily@mail.ru>
2021-06-30 17:05:26 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,100 @@
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    const select = document.getElementById('currency'),
+        output = document.getElementById('output'),
+        url = 'http://api.exchangeratesapi.io/v1/latest?access_key=',
+        key = 'b043fdbd1730a4e0158bab349a2a64e4',
+        symbols = '&symbols=USD,RUB';
+
+    const init = (target) => {
+        output.innerHTML = '';
+        let addSpan = '';
+        if (target.value === 'USD') {
+            addSpan = 'Доллар США (USD)';
+        } else if (target.value === 'EUR') {
+            addSpan = 'Евро (EUR)';
+        }
+        const outputConverter = document.createElement('div');
+        const outputConverterBack = document.createElement('div');
+        outputConverter.classList.add('output-converter');
+        outputConverterBack.classList.add('output-converter-back');
+        outputConverter.innerHTML = `<input type="text" class="currency currency-output"><span>${addSpan}</span>
+            <input type="text" class="converter converter-output" disabled><span>Российский рубль (RUB)</span>
+            <button class="btn-converter-output">Конвертировать</button>`;
+        outputConverterBack.innerHTML = `<input type="text" class="currency currency-back"><span>Российский рубль (RUB)</span>
+            <input type="text" class=" converter-back" disabled><span>${addSpan}</span>
+            <button class="btn-converter-back">Конвертировать</button>`;
+        output.append(outputConverter);
+        output.append(outputConverterBack);
+        output.addEventListener('input', event => {
+            checkInput(event);
+        });
+        getData(target.value);
+    };
+
+    const checkInput = (event) => {
+        const target = event.target;
+        if (target.value) {
+            if (target.closest('input')) {
+                target.value = target.value.replace(/[^0-9]/g, '');
+            } else {
+                return;
+            }
+        }
+    };
+
+    const getCurrency = (data, currencyId) => {
+        const { USD, RUB } = data.rates;
+        const currencyOutput = document.querySelector('.currency-output');
+        const converterOutput = document.querySelector('.converter-output');
+        const currencyBack = document.querySelector('.currency-back');
+        const converterBack = document.querySelector('.converter-back');
+        converterOutput.value = 0;
+        converterBack.value = 0;
+        output.addEventListener('click', event => {
+            const target = event.target;
+            if (currencyId === 'USD') {
+                if (target.matches('.btn-converter-output')) {
+                    converterOutput.value = parseFloat((currencyOutput.value * (RUB / USD)).toFixed(2));
+                } else if (target.matches('.btn-converter-back')) {
+                    converterBack.value = parseFloat((currencyBack.value / (RUB / USD)).toFixed(2));
+                } else {
+                    return;
+                }
+            }
+            if (currencyId === 'EUR') {
+                if (target.matches('.btn-converter-output')) {
+                    converterOutput.value = parseFloat((currencyOutput.value * RUB).toFixed(2));
+                } else if (target.matches('.btn-converter-back')) {
+                    converterBack.value = parseFloat((currencyBack.value / RUB).toFixed(2));
+                } else {
+                    return;
+                }
+            }
+        });
+    };
+
+    const getData = (currencyId) => {
+        fetch(`${url}${key}${symbols}`, {
+            method: 'GET',
+            mode: 'cors',
+            body: JSON.stringify()
+        })
+            .then(response => {
+                if (response.status !== 200) {
+                    throw new Error('status network not 200');
+                }
+                return (response.json());
+            })
+            .then(data => {
+                getCurrency(data, currencyId);
+            })
+            .catch(error => console.log(error));
+    };
+
+    select.addEventListener('change', event => {
+        const target = event.target;
+        init(target);
+    });
+});
